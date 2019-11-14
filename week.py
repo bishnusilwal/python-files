2d0c604dbb50b9dab7e560a33128e952361ea0e2
macbook <macbook@macbooks-MacBook-Air.local>
2019-11-14 18:42:27 +0545

    this is the most important programming language

---
+++ week.py
@@ -0,0 +1,17 @@
+a=int(input("Enter any number "))
+if a==1:
+    print("sunday","it's  a holiday")
+elif a==2:
+    print("munday")
+elif a==3:
+    print("tuesday")
+elif a==4:
+    print("wed")
+elif a==5:
+    print("thus")
+elif a==6:
+    print("fri")
+elif a==7:
+    print("saturday","it's a holiday")
+else:
+ print("exit")
